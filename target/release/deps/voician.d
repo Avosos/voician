f9b8f658d0dfe948119ea9a3726f91d7aec7674a
Marius <mariusgutschalk@gmail.com>
2026-03-12 14:28:02 +0100

    Update dependency tracking to include strudel.rs in build files

--- target/release/deps/voician.d
+++ target/release/deps/voician.d
@@ -1,6 +1,6 @@
-C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.d: src\main.rs src\analysis.rs src\audio.rs src\crepe.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs
+C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.d: src\main.rs src\analysis.rs src\audio.rs src\crepe.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs src\strudel.rs
 
-C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.exe: src\main.rs src\analysis.rs src\audio.rs src\crepe.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs
+C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.exe: src\main.rs src\analysis.rs src\audio.rs src\crepe.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs src\strudel.rs
 
 src\main.rs:
 src\analysis.rs:
@@ -11,3 +11,4 @@ src\gui.rs:
 src\midi.rs:
 src\pitch.rs:
 src\state.rs:
+src\strudel.rs:
