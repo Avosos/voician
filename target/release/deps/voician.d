a9ee9dd7807c3d757d73acc72b7e0faa7982ee30
Marius <mariusgutschalk@gmail.com>
2026-03-12 14:13:22 +0100

    Update dependency tracking and remove obsolete binary files

--- target/release/deps/voician.d
+++ target/release/deps/voician.d
@@ -1,10 +1,11 @@
-C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.d: src\main.rs src\analysis.rs src\audio.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs
+C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.d: src\main.rs src\analysis.rs src\audio.rs src\crepe.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs
 
-C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.exe: src\main.rs src\analysis.rs src\audio.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs
+C:\Users\Marius\Desktop\projects\voician\target\release\deps\voician.exe: src\main.rs src\analysis.rs src\audio.rs src\crepe.rs src\engine.rs src\gui.rs src\midi.rs src\pitch.rs src\state.rs
 
 src\main.rs:
 src\analysis.rs:
 src\audio.rs:
+src\crepe.rs:
 src\engine.rs:
 src\gui.rs:
 src\midi.rs:
